638dc305487c99848516bfeb1e4a686ea883cc91
Adir Burke on Laptop <adir@hanave.com>
2020-09-10 14:09:32 +1000

    Saving packae.swift

--- Package.swift
+++ Package.swift
@@ -6,13 +6,9 @@ let package = Package(name: "PerfectSMTP",
 	],
 	products: [.library(name: "PerfectSMTP",targets: ["PerfectSMTP"]),],
     dependencies: [
-<<<<<<< HEAD
-	.package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", from: "5.0.0"),
-=======
 		.package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", from: "5.0.0"),
 		.package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", from: "4.0.0"),
 		.package(url: "https://github.com/PerfectlySoft/Perfect-MIME.git", from: "1.0.0"),
->>>>>>> a051f336d1c63b37ba1eea5685db1aa57a17224d
 	],
     targets: [
         .target(
